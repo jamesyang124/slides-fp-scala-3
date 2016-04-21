5ce600b816a9a6bf9353963ef27171525418fd10
James Yang <james_yang@htc.com>
2016-04-21 14:32:28 +0800

    add bundle to launch up express

--- web.js
+++ web.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser()); // pull information from html in POST
+app.use(methodOverride());  // simulate DELETE and PUT
+//app.use(express.logger());
 app.use("/", express.static(__dirname));
 
 var port = process.env.PORT || 5000;
